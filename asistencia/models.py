0947fe20ac54bf257837b95af03bda22a317da26
Angie Benavides <angiebenavides1809@gmail.com>
2026-04-16 11:43:00 -0500

    Implementa aplicación asistencia con modelo, formulario, vistas, URLs, plantillas y registro actualizadas

--- asistencia/models.py
+++ asistencia/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Asistencia(models.Model):
     nombre_completo = models.CharField(max_length=150)
-    documento = models.CharField(max_length=50)
-    correo = models.EmailField()
-    fecha = models.DateField()
-    hora_ingreso = models.TimeField()
-    hora_salida = models.TimeField()
+    documento_de_identidad = models.CharField(max_length=50)
+    correo_electronico = models.EmailField()
+    fecha_de_asistencia = models.DateField()
+    hora_de_ingreso = models.TimeField()
+    hora_de_salida = models.TimeField()
     presente = models.BooleanField()
     observaciones = models.TextField(blank=True, null=True)
 
